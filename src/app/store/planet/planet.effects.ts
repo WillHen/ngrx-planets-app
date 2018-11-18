48f719454e0d0f477f07f915c197cef69298d6c6
William Henshaw <williamhenshaw@macbook.home>
2018-11-17 19:57:48 -0600

    fix spacing

--- src/app/store/planet/planet.effects.ts
+++ src/app/store/planet/planet.effects.ts
@@ -9,21 +9,21 @@ import { PlanetsService } from "../../services/planets.service";
 
 @Injectable()
 export class PlanetEffects {
-	@Effect()
-	fetchPlanets$: Observable<Action> = this.actions$.pipe(
-		ofType(PlanetActions.FETCH_PLANETS),
-		switchMap(query => {
-			return this._planetsService.getPlanets().pipe(
-				map(planets => {
-					return new PlanetActions.FetchPlanetsDone(planets);
-				})
-			);
-		})
-	);
-	// catch(() => of(new FoodActions.FetchFoodFail())
+    @Effect()
+    fetchPlanets$: Observable<Action> = this.actions$.pipe(
+        ofType(PlanetActions.FETCH_PLANETS),
+        switchMap(query => {
+            return this._planetsService.getPlanets().pipe(
+                map(planets => {
+                    return new PlanetActions.FetchPlanetsDone(planets);
+                })
+            );
+        })
+    );
+    // catch(() => of(new FoodActions.FetchFoodFail())
 
-	constructor(
-		private actions$: Actions,
-		private _planetsService: PlanetsService
-	) {}
+    constructor(
+        private actions$: Actions,
+        private _planetsService: PlanetsService
+    ) {}
 }
